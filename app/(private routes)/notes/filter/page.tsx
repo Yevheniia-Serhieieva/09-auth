3c0d7139ba13824d81c610a5614915702d09c523
Yevheniia Serhieieva <y.o.serhieieva@gmail.com>
2025-11-14 14:58:27 +0200

    all

--- app/(private routes)/notes/filter/page.tsx
+++ app/(private routes)/notes/filter/page.tsx
@@ -1,13 +1,20 @@
 import NoteList from "@/components/NoteList/NoteList";
+import { getNotes } from "@/lib/api/api";
 import { Note } from "@/types/note";
 
-const AllNotesPaage = ({ notes }: { notes: Note[] }) => {
+export default async function AllNotesPaage() {
+  let notes: Note[] = [];
+
+  try {
+    const responce = await getNotes();
+    notes = responce?.notes || [];
+  } catch (err) {
+    console.error(err);
+  }
   return (
     <div>
       <h1>All Notes</h1>
       {notes.length > 0 && <NoteList notes={notes} />}
     </div>
   );
-};
-
-export default AllNotesPaage;
+}
